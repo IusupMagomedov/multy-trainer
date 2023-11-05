d3b1bab55e1304404554502f35b776c6ebfb2123
IusupMagomedov <yusub1988@gmail.com>
2023-11-05 12:31:11 +0300

    Add: Timer progressBar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import LinearProgress from '@mui/material/LinearProgress';
 
 
 
@@ -16,6 +17,16 @@ import './App.css';
 
 function App() {
   const [multArr, setMultArr] = useState([]);
+  const [timer, setTimer] = useState(50);
+  const [multiplicand, setMulplicand] = useState();
+  const [multiplier, setMultiplier] = useState()
+
+  useEffect(() => {
+    setTimeout(() => {
+      timer > 0 && setTimer( timer - 1 )
+    }, 100);
+  }, [timer])
+  
   
   useEffect(() => {
     const arr = [];
@@ -31,18 +42,28 @@ function App() {
     }
     setMultArr(arr);
   })
+  useEffect(() => {
+    setMultiplier(2);
+    setMulplicand(2);
+  
+  }, [multArr])
+  
 
   return (
     <Box >
       <Grid container >
         <Grid item xs={12}>
           <Typography align='center' variant="h1" gutterBottom>
-            Multiplication table trainer
+            Multiplication table trainer 
+          </Typography>
+        </Grid><Grid item xs={12}>
+          <Typography align='center' variant="h1" gutterBottom>
+            <LinearProgress variant="determinate" value={timer * 2} />
           </Typography>
         </Grid>
         <Grid item xs={2.4}>
           <Typography align='center' variant="h3" gutterBottom>
-            3
+            {multiplier}
           </Typography>
         </Grid>
         <Grid item xs={2.4}>
@@ -52,7 +73,7 @@ function App() {
         </Grid>
         <Grid item xs={2.4}>
           <Typography align='center' variant="h3" gutterBottom>
-            3
+            {multiplicand}
           </Typography>
         </Grid>
         <Grid item xs={2.4}>
